f7d225676c00d1a218c2549d404be2ef2648a8a0
Egor Kuziakov <egor.kuziakov@gmail.com>
2025-12-09 17:06:40 +0300

    Add neo-tree

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+      require('neo-tree').setup {
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+        },
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+            never_show = {
+	      '.git',
+	      '.DS_Store',
+	    },
+          },
+        },
+      }
+      vim.keymap.set('n', '<leader>ee', ':Neotree toggle filesystem left<CR>', { desc= 'Open [E]xplorer' })
+      vim.keymap.set('n', '<leader>ef', ':Neotree toggle filesystem reveal left<CR>', { desc= 'Open [E]xplorer at [F]ile' })
+    end
+  }
+}
