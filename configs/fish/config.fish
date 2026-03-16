fb593c62816bcecc136c2c83ad50c42bd9c4a5da
Egor Kuziakov <egor.kuziakov@gmail.com>
2026-03-16 10:21:18 +0300

    Add orbstack to fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -20,3 +20,7 @@ zoxide init fish | source
 # fzf setup
 fzf --fish | source
 set -x XDG_CONFIG_HOME $HOME/.config
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
